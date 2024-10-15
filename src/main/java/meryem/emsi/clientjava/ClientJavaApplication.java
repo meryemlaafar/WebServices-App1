3f1e89fbc0b36db1ffcd9cc0e6d9525bc534171a
Meryem Laafar <LAAFAR_MERYEM@emsi-edu.ma>
2024-10-15 18:19:48 +0100

    ClientJava commit

---
+++ src/main/java/meryem/emsi/clientjava/ClientJavaApplication.java
@@ -0,0 +1,13 @@
+package meryem.emsi.clientjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/meryem/emsi/clientjava/ClientJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package meryem.emsi.clientjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
